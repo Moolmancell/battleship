045c3b6b8bfe2dc35e2961cb11eb902b73c6df71
Moolmancell <richmondclaudebodot@gmail.com>
2024-11-19 14:43:11 +0800

    Add HTML plugin

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: 'Battleship',
+        }),
+    ],
+};
