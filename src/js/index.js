3e23e93b317653daa115e63030b6e9fadcfcb1ea
Moolmancell <richmondclaudebodot@gmail.com>
2024-11-24 18:03:47 +0800

    Add renderGameboard()

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import "../index.css";
+import interact from 'interactjs'
 import { Gameboard } from "./gameboard";
 import { Player } from "./player";
 import { Ship } from "./ship";
+import { renderGameboard } from "./render-gameboard";
+
+renderGameboard("gameboard-player-1");
+
+import "./drag-place-ship";
